58bd882b39db4d003d85482751d55c44fac6b16c
sg1108951 <sg1108951@gmail.com>
2020-10-18 02:37:28 +0530

    Toolbar

--- shopaholic/src/app/app.module.ts
+++ shopaholic/src/app/app.module.ts
@@ -7,6 +7,18 @@ import { allAppRoutes } from './routes';
 import { AppComponent } from './app.component';
 import{ TitlebarComponent } from './homepage/title-bar/title-bar.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +26,14 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(allAppRoutes)
+    RouterModule.forRoot(allAppRoutes),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSliderModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- shopaholic/src/app/homepage/title-bar/title-bar.component.ts
+++ shopaholic/src/app/homepage/title-bar/title-bar.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
 
 export class TitlebarComponent{
 
-    title : string = 'Shopaholic'
+  value = 'Clear me';
 
 }
